ecc64edde7da268e62141af764e52db72235f1ad
MaximePicard <maximepicard101@icloud.com>
2020-10-05 15:02:08 -0400

    Realisation du menu responsive

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburgers = document.querySelectorAll( ".hamburger");
+    var menumobile = document.querySelector(".nav-primary-mobile");
+
+    for(let i = 0;  i < hamburgers.length; i++){
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener("click", openMenu);
+    }
+
+    function openMenu(evt){
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if(cible.classList.contains("open")){
+            cible.classList.remove("open");
+            menumobile.classList.remove("open");
+        }else{
+            cible.classList.add("open");
+            menumobile.classList.add("open");
+        }
+    }
+
 });
